0bb5fa4e3f0cbcac19a029a7f770ca9441965fd1
Tretyakov Dmitry <tretyakov.dmi@gmail.com>
2018-08-08 02:07:14 +0300

    api host was updated

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './containers/App';
 import Store from './store';
-// require('dotenv').config();
 
 render(
     <Provider store={Store}>
